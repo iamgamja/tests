6601743dcf52e6287ca5f4da6760fac984f3a6c9
iamgamja <hmmthinking@naver.com>
2021-06-18 15:31:32 +0900

    Update sosu_calculator.js

--- sosu_calculator.js
+++ sosu_calculator.js
@@ -1,13 +1,32 @@
-let N = 5;
 let a = 1;
-let b = 3;
+let b = 3; // b가 나누는 수. 항상 b로 나눈다.
+
+let finishied = false;
+
+// let data = {
+//   // 1: 2
+//   // 나머지가 0일때 b로 나누고 나면 나머지가 2가 된다.
+// }
+
+let last_namugi = 1;
 
 onmessage = function(event) {
-  let li = event.data;
-  a = Number(li[0]);
-  b = Number(li[1]);
+  a = parseInt(event.data[0]);
+  b = parseInt(event.data[1]);
+  finishied = false;
+//   data = {};
+  postMessage(`${D(a, b)}.`); // 초기화, ex) '0.'
+  last_namugi = d(a,b)
 }
 
-let timer = setInterval(function() {
-  postMessage(a/b);
-}, 1000);
+function D(a, b) {  return parseInt(a/b)  }
+function d(a, b) {  return a%b  }
+
+while (!finishied) {
+  postMessage(D(last_namugi, b));
+  last_namugi = d(last_namugi, b);
+}
+
+// let timer = setInterval(function() {
+//   postMessage(a/b);
+// }, 1000);
