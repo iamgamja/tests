9a26d5fb12998cd504f26e461ca048846821e039
iamgamja <hmmthinking@naver.com>
2021-06-18 15:17:21 +0900

    Update sosu_calculator.js

--- sosu_calculator.js
+++ sosu_calculator.js
@@ -1,27 +1,13 @@
 let N = 5;
-let a = 10;
-let b = 2;
+let a = 1;
+let b = 3;
 
 onmessage = function(event) {
   let li = event.data;
-  a = li[0];
-  b = li[1];
+  a = Number(li[0]);
+  b = Number(li[1]);
 }
 
-// search: while (true) {
-// //   n += 1;
-// //   for (var i = 2; i <= Math.sqrt(n); i += 1) {
-// //     if (n % i == 0) {
-// //       continue search;
-// //     }
-// //     postMessage(n);
-// //   }
-  
-//   for (let i=0; i<N; i++) {
-//     postMessage(i);
-//   }
-// }
-  
 let timer = setInterval(function() {
-  postMessage([a, b]);
+  postMessage(a/b);
 }, 1000);
