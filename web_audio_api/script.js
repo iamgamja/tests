da2f3a2a78d44900b75a7ee0ce0c2f5e2a7860b1
iamgamja <hmmthinking@naver.com>
2022-02-22 17:17:27 +0900

    .

--- web_audio_api/script.js
+++ web_audio_api/script.js
@@ -23,6 +23,51 @@ function main() {
   let bufferLength = analyser.frequencyBinCount;
   let dataArray = new Uint8Array(bufferLength);
 
-  analyser.getByteTimeDomainData(dataArray);
+  // analyser.getByteTimeDomainData(dataArray);
+
+  let canvas = document.querySelector('canvas');
+  let canvasCtx = canvas.getContext('2d');
+
+  const WIDTH = 300;
+  const HEIGHT = 150;
+
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  function draw() {
+    const drawVisual = requestAnimationFrame(draw); // 루프
+
+    analyser.getByteTimeDomainData(dataArray); // 오디오 정보 얻기
+
+    // 배경 채우기
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    // 선 그리기
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasCtx.beginPath();
+
+    let sliceWidth = WIDTH / bufferLength; // 캔버스 길이를 데이터 길이로 나누기
+    let x = 0; // 그릴 x좌표
+
+    for(let i = 0; i < bufferLength; i++) {
+
+      let v = dataArray[i] / 128.0;
+      let y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    // 선 끝내기
+    canvasCtx.lineTo(WIDTH, HEIGHT/2);
+    canvasCtx.stroke();
+  }
+  draw(); // 루프 시작
 
 }
