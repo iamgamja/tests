d6531a47fe6cbf9edec442b50ba1f3545b7f89d2
iamgamja <hmmthinking@naver.com>
2022-02-23 11:16:38 +0900

    edit js.

--- web_audio_api/script.js
+++ web_audio_api/script.js
@@ -32,16 +32,13 @@ document.querySelector('input').addEventListener('change', function() {
   });
 
   analyser.fftSize = 2048;
-  let bufferLength = analyser.frequencyBinCount;
-  let dataArray = new Uint8Array(bufferLength);
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
 
-  let canvas = document.querySelector('canvas');
-  let canvasCtx = canvas.getContext('2d');
+  const canvas = document.querySelector('canvas');
+  const canvasCtx = canvas.getContext('2d');
 
-  const WIDTH = canvas.width;
-  const HEIGHT = canvas.height;
-
-  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
   function draw() {
     const drawVisual = requestAnimationFrame(draw); // 루프
@@ -50,20 +47,20 @@ document.querySelector('input').addEventListener('change', function() {
 
     // 배경 채우기
     canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
     // 선 그리기
     canvasCtx.lineWidth = 2;
     canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
     canvasCtx.beginPath();
 
-    let sliceWidth = WIDTH / bufferLength; // 캔버스 길이를 데이터 길이로 나누기
+    let sliceWidth = canvas.width / bufferLength; // 캔버스 길이를 데이터 길이로 나누기
     let x = 0; // 그릴 x좌표
 
     for(let i = 0; i < bufferLength; i++) {
 
       let v = dataArray[i] / 128.0;
-      let y = v * HEIGHT/2;
+      let y = v * canvas.height/2;
 
       if(i === 0) {
         canvasCtx.moveTo(x, y);
@@ -75,7 +72,7 @@ document.querySelector('input').addEventListener('change', function() {
     }
 
     // 선 끝내기
-    canvasCtx.lineTo(WIDTH, HEIGHT/2);
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
     canvasCtx.stroke();
   }
   draw(); // 루프 시작
