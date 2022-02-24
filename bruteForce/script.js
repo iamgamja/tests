adde8579b6df250ca7c99e3eb9baa9380d7e131f
iamgamja <hmmthinking@naver.com>
2022-02-24 17:11:57 +0900

    add 부르트포스 알고리즘

---
+++ bruteForce/script.js
@@ -0,0 +1,10 @@
+$('button').on('click', function() {
+  const F = new Function( 'N', $('textarea').val() );
+  
+  for (let i = $('#start').val(); i <= $('end').val(); i++) {
+    if (F(i)) {
+      $('output').val(i);
+      return;
+    }
+  }
+});
