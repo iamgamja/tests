ede26f766f020291d13ca20691cdcb3fc497b71d
iamgamja <hmmthinking@naver.com>
2022-02-25 15:38:27 +0900

    edit

--- bruteForce/script.js
+++ bruteForce/script.js
@@ -1,17 +1,27 @@
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+
 $('button').on('click', function() {
   const F = new Function( 'N', $('textarea').val() );
   
   const start = +$('#start').val();
   const end = +$('#end').val();
 
-  $('progress').attr('max', end-start+1);
+  const total = end-start+1;
+
+  $('progress').attr('max', total);
+  $('progress').attr('value', 0);
 
   for (let i = start; i <= end; i++) {
     if (F(i)) {
       $('output').val(i);
+      $('progress').attr('value', total);
       return;
     } else {
-      $('progress').attr('value', i-start+1);
+      const p = i-start+1;
+      if (total/10 <= p && p <= total/10+1) {
+        $('progress').attr('value', p);
+        await sleep(100);
+      }
     }
   }
 });
