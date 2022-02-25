51e5c20626be318babf84a9742d1004bbbd892bd
iamgamja <hmmthinking@naver.com>
2022-02-25 13:48:33 +0900

    edit.

--- bruteForce/script.js
+++ bruteForce/script.js
@@ -4,12 +4,14 @@ $('button').on('click', function() {
   const start = +$('#start').val();
   const end = +$('#end').val();
 
+  $('progress').attr('max', end-start+1);
+
   for (let i = start; i <= end; i++) {
     if (F(i)) {
       $('output').val(i);
       return;
     } else {
-      console.log(`not ${i}`);
+      $('progress').attr('value', (i-start+1) / (end-start+1));
     }
   }
 });
