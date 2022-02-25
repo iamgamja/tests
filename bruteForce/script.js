b8da0fbacbf5fe7d49452b137451efd5c78ae331
iamgamja <hmmthinking@naver.com>
2022-02-25 13:38:42 +0900

    edit.

--- bruteForce/script.js
+++ bruteForce/script.js
@@ -1,10 +1,15 @@
 $('button').on('click', function() {
   const F = new Function( 'N', $('textarea').val() );
   
-  for (let i = +$('#start').val(); i <= +$('end').val(); i++) {
+  const start = +$('#start').val();
+  const end = +$('end').val();
+
+  for (let i = start; i <= end; i++) {
     if (F(i)) {
       $('output').val(i);
       return;
+    } else {
+      console.log(`not ${i}`);
     }
   }
 });
